d9e6490ebb1862900429b0d60e7a08e4c7431321
Julian Tescher <virulent@gmail.com>
2013-09-30 23:51:32 -0700

    Initial Rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+CarrierwaveImageoptimizerExample::Application.config.secret_key_base = '0a2edc34a02acd5a118f772233b939937287d6bb3c6c241782dbb5e6bc22597cc5c60928825e9b1b6727adeb8b6b70c771152054c5a6f58cc22bb62efea0808d'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+CarrierwaveImageoptimizerExample::Application.config.session_store :cookie_store, key: '_carrierwave_imageoptimizer_example_session'
